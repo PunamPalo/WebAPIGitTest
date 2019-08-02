d94c3dda2df7bb61b45f5def9cb50784da45054d
PunamPalo <52999358+PunamPalo@users.noreply.github.com>
2019-08-02 12:10:23 +0200

    TestGIT

---
+++ WebAPI MVC/Controllers/CalculatePriceController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_MVC.Controllers
+{
+    public class CalculatePriceController : ApiController
+    {
+
+        
+        [HttpGet]
+        public float GetDetails(string product, int qty, float price)
+        { 
+            // i did this in static way just because we have to set all those price and offfers in master tables //
+            float finalprice;
+            if (product == "A" && qty == 3)
+            {
+                finalprice = 3;
+            }
+            else
+            {
+                finalprice = price;
+            }
+            float result = qty * finalprice;
+            float ttlresult = 0;
+            ttlresult = result + ttlresult;
+            return ttlresult;
+        }
+
+     
+        }
+}
---
+++ WebAPI MVC/Models/ItemDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_MVC.Models
+{
+    public class ItemDetails
+    {
+        public string product { get; set; }
+        public int qty { get; set; }
+        public decimal Price { get; set; }
+    }
+}
